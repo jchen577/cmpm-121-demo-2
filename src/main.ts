ee17fcf37f37583fbfd64efac244a14f367092f6
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-10-14 00:59:06 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+//const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//document.title = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+const appTitle = document.createElement("h1");
+appTitle.innerHTML = "DRAW!";
+app.append(appTitle);
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "white";
+ctx.fillRect(10, 10, 256, 256);
