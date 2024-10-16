01f0445358394529cff13dfa5141d6373a526ba6
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-10-15 20:24:59 -0700

    D2 Part1 Final Update, Code Clean, Bug Fixes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import "./style.css";
 
-//const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
-//document.title = APP_NAME;
-//app.innerHTML = APP_NAME;
 
 const appTitle = document.createElement("h1");
 appTitle.innerHTML = "DRAW!";
@@ -41,14 +37,27 @@ canvas?.addEventListener(
         }
       }
     } else {
-      for (let i = 0; i < holder.length; i++) {
-        drawLine(ctx, holder[i][0], holder[i][1], holder[i][2], holder[i][3]);
+      ctx.fillRect(0, 0, 256, 256);
+      for (let k = 0; k < holder.length; k++) {
+        drawLine(ctx, holder[k][0], holder[k][1], holder[k][2], holder[k][3]);
+      }
+      for (let i = 0; i < points.length; i++) {
+        for (let j = 0; j < points[i].length; j++) {
+          drawLine(
+            ctx,
+            points[i][j][0],
+            points[i][j][1],
+            points[i][j][2],
+            points[i][j][3]
+          );
+        }
       }
     }
   },
   false
 );
 canvas?.addEventListener("mousedown", (pos: MouseEvent) => {
+  undoPoints = [];
   x = pos.offsetX;
   y = pos.offsetY;
   isDrawing = true;
@@ -56,8 +65,6 @@ canvas?.addEventListener("mousedown", (pos: MouseEvent) => {
 
 canvas?.addEventListener("mousemove", (pos: MouseEvent) => {
   if (isDrawing) {
-    //drawLine(ctx, x, y, pos.offsetX, pos.offsetY);
-    //points.push([x, y, pos.offsetX, pos.offsetY, 0]);
     holder.push([x, y, pos.offsetX, pos.offsetY]);
     canvas.dispatchEvent(drawChangeEvent);
     x = pos.offsetX;
@@ -67,10 +74,8 @@ canvas?.addEventListener("mousemove", (pos: MouseEvent) => {
 
 canvas?.addEventListener("mouseup", (pos: MouseEvent) => {
   if (isDrawing) {
-    //drawLine(ctx, x, y, pos.offsetX, pos.offsetY);
     holder.push([x, y, pos.offsetX, pos.offsetY]);
     points.push(holder);
-    //points.push([x, y, pos.offsetX, pos.offsetY, tracker]);
     isDrawing = false;
     canvas.dispatchEvent(drawChangeEvent);
     x = 0;
@@ -116,8 +121,10 @@ function drawLine(
 }
 function clearCanvas(context: CanvasRenderingContext2D) {
   context.fillRect(0, 0, 256, 256);
+  for (let i = 0; i < points.length; i++) {
+    undoPoints.push(points[i]);
+  }
   points = [];
-  undoPoints = [];
 }
 function undo() {
   if (points.length > 0) {
